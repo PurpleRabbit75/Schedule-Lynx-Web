58d0dbea1f658a94066d5801b6b83a67c7ad9c3a
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-01-11 08:45:52 -0600

    added uploader

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import streamlit as st
+from io import StringIO
 
 st.title("Schedule Lynx")
+st.write("Welcome to Schedule Lynx! Upload your person.json files to get started.")
+
+
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
