bf9fd66a21d42558e4b6d9303d20db41483beb17
PurpleRabbit75 <abrahambaker104@gmail.com>
2026-01-11 09:39:24 -0600

    Fixed basic JSON uploader

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import streamlit as st
-from io import StringIO
+import json
 
 st.title("Schedule Lynx")
 st.write("Welcome to Schedule Lynx! Upload your person.json files to get started.")
 
+# 1. Create the file uploader widget
+# type='json' restricts the user to selecting only .json files
+uploaded_file = st.file_uploader("Choose a JSON file", type=["json"])
 
-
-uploaded_file = st.file_uploader("Choose a file")
+# 2. Check if a file has been uploaded
 if uploaded_file is not None:
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    with open(uploaded_file, 'r') as file:
-        COLORS = [tuple(color) for color in list(st.json.load(file).values())]
-    st.write(COLORS)
+    try:
+        # 3. Load the JSON data
+        # Streamlit's file_uploader returns a file-like object, 
+        # so we can simply pass it to json.load()
+        data = json.load(uploaded_file)
+        
+        # 5. Display the data
+        # st.json() creates an interactive, collapsible view of the data
+        st.subheader(uploaded_file.name)
+        st.json(data)
+        
+        # You can now access data as a standard Python dictionary/list
+        # Example: st.write(data['some_key'])
+        
+    except json.JSONDecodeError:
+        st.error("The file you uploaded is not valid JSON.")
